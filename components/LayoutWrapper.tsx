e272d7c266e2b7f8490ad1d934571e2a77fb6b28
Saurabh Prakash <saurabhp75@gmail.com>
2022-05-16 19:04:22 +0530

    update logo

--- components/LayoutWrapper.tsx
+++ components/LayoutWrapper.tsx
@@ -1,6 +1,8 @@
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
-import Logo from '@/data/logo.svg'
+// import Logo from '@/data/logo.svg'
+import Image from 'next/image'
+import logoimage from '../public/static/images/logo.png'
 import Link from './Link'
 import SectionContainer from './SectionContainer'
 import Footer from './Footer'
@@ -21,7 +23,16 @@ const LayoutWrapper = ({ children }: Props) => {
             <Link href="/" aria-label={siteMetadata.headerTitle}>
               <div className="flex items-center justify-between">
                 <div className="mr-3">
-                  <Logo />
+                  <Image
+                    src={logoimage}
+                    alt="Picture of the author"
+                    // layout='fill'
+                    width={70}
+                    height={70}
+                    // blurDataURL="data:..." automatically provided
+                    // placeholder="blur" // Optional blur-up while loading
+                  />
+                  {/* <Logo /> */}
                 </div>
                 {typeof siteMetadata.headerTitle === 'string' ? (
                   <div className="hidden h-6 text-2xl font-semibold sm:block">
